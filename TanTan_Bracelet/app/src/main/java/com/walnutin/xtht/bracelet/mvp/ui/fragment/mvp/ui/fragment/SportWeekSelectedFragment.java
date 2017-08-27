d9ca8f19bf1d690fa5ceef1f366e6e309c95c146
hongtai.lei <530316112@qq.com>
2017-08-27 23:18:56 +0800

    fix 8-27

--- TanTan_Bracelet/app/src/main/java/com/walnutin/xtht/bracelet/mvp/ui/fragment/mvp/ui/fragment/SportWeekSelectedFragment.java
+++ TanTan_Bracelet/app/src/main/java/com/walnutin/xtht/bracelet/mvp/ui/fragment/mvp/ui/fragment/SportWeekSelectedFragment.java
@@ -136,7 +136,8 @@ public class SportWeekSelectedFragment extends BaseFragment<SportWeekSelectedPre
             currentIndexItem = position;
             currentDate = item.getDate();
 
-            if (isNow(currentDate)) {
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");//设置日期格式
+            if (isSameDate(currentDate, df.format(new Date()))) {
                 viewpager.setScrollble(false);
             } else {
                 viewpager.setScrollble(true);
@@ -144,6 +145,45 @@ public class SportWeekSelectedFragment extends BaseFragment<SportWeekSelectedPre
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public boolean isSameDate(String date1, String date2)
+    {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date d1 = null;
+        Date d2 = null;
+        try
+        {
+            d1 = format.parse(date1);
+            d2 = format.parse(date2);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setFirstDayOfWeek(Calendar.MONDAY);//西方周日为一周的第一天，咱得将周一设为一周第一天
+        cal2.setFirstDayOfWeek(Calendar.MONDAY);
+        cal1.setTime(d1);
+        cal2.setTime(d2);
+        int subYear = cal1.get(Calendar.YEAR) - cal2.get(Calendar.YEAR);
+        if (subYear == 0)// subYear==0,说明是同一年
+        {
+            if (cal1.get(Calendar.WEEK_OF_YEAR) == cal2.get(Calendar.WEEK_OF_YEAR))
+                return true;
+        }
+        else if (subYear == 1 && cal2.get(Calendar.MONTH) == 11) //subYear==1,说明cal比cal2大一年;java的一月用"0"标识，那么12月用"11"
+        {
+            if (cal1.get(Calendar.WEEK_OF_YEAR) == cal2.get(Calendar.WEEK_OF_YEAR))
+                return true;
+        }
+        else if (subYear == -1 && cal1.get(Calendar.MONTH) == 11)//subYear==-1,说明cal比cal2小一年
+        {
+            if (cal1.get(Calendar.WEEK_OF_YEAR) == cal2.get(Calendar.WEEK_OF_YEAR))
+                return true;
+        }
+        return false;
     }
 
     private String getLastWeekToday(String da) {
